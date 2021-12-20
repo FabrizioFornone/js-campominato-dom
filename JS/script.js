4107a3ccdbd7d20de1d3ecc89247c116a503db3c
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-20 16:44:34 +0100

    Push bombs created

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,20 @@
-/* L’utente indica un livello di difficoltà (3 pulsanti)
- in base al quale viene generata una griglia di gioco quadrata,
-  in cui ogni cella contiene un numero tra quelli compresi in un range:
+/* L’utente indica un livello di difficoltà in base al quale viene
+ generata una griglia di gioco quadrata, in cui ogni cella contiene
+  un numero tra quelli compresi in un range:
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
+Il computer deve generare 16 numeri casuali nello stesso range
+ della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l’utente clicca su una cella: se il numero è presente
+ nella lista dei numeri generati - abbiamo calpestato una bomba - 
+ la cella si colora di rosso e la partita termina, altrimenti la cella
+  cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge
+ il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, 
+cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. */
 
 // Funzione di generazione quadrati easy, "container" sarà il mio stampHtml
 
@@ -66,14 +76,24 @@ function hardBoxGenerator(container, insideNumber) {
   });
 }
 
+// funziona che genera una bomba con un numero da 1 a squaresNumbers
+
+function randomBomb(squaresNumbers) {
+  const result = Math.floor(Math.random() * (squaresNumbers + 1 - 1)) + 1;
+  // Return per poterla portare fuori
+
+  return result;
+}
+
 // funzione "newGame" generator, "difficultyGenerator è a sua volta una funzione"
 
-function newGame(stampGrill, numberOfSquare, difficultyGenerator) {
+function newGame(stampGrill, numberOfSquares, difficultyGenerator) {
   stampGrill.innerHTML = "";
-  for (i = 1; i <= numberOfSquare; i++) {
+  for (i = 1; i <= numberOfSquares; i++) {
     difficultyGenerator(stampGrill, i);
   }
 }
+
 
 // dichiarazione costanti associate ad elementi del DOM
 
@@ -89,34 +109,58 @@ const button3 = document.getElementById("js-btn-hook3");
 
 button1.addEventListener("click", function () {
   newGame(stampHtml, 100, easyBoxGenerator);
+  //inserisco in un array vuoto 16 bombe casuali non ripetute
+  let arrayNumbers = [];
+  /* fai il ciclo FINCHE' l'array non diventa
+   di 15 elementi + 1 elemento extra aggiunto sotto e si chiude */
+  while (arrayNumbers.length < 16) {
+    // dichiaro cost random associata alla funzione
+    const random = randomBomb(100);
+    // Se l'array non include questo numero
+    if (!arrayNumbers.includes(random)) {
+      // PUSHALO nell'array
+      arrayNumbers.push(random);
+    }
+  }
+  console.log(arrayNumbers);
 });
 
 // button2 genera ciclo for per creare griglia media
 
 button2.addEventListener("click", function () {
   newGame(stampHtml, 81, mediumBoxGenerator);
+  // dichiaro un array vuoto
+  let arrayNumbers = [];
+  /* fai il ciclo FINCHE' l'array non diventa
+   di 15 elementi + 1 elemento extra aggiunto sotto e si chiude */
+  while (arrayNumbers.length < 16) {
+    // dichiaro cost random associata alla funzione
+    const random = randomBomb(81);
+    // Se l'array non include questo numero
+    if (!arrayNumbers.includes(random)) {
+      // PUSHALO nell'array
+      arrayNumbers.push(random);
+    }
+  }
+  console.log(arrayNumbers);
 });
 
 // button3 genera ciclo for per creare griglia difficile
 
 button3.addEventListener("click", function () {
   newGame(stampHtml, 49, hardBoxGenerator);
+  // dichiaro un array vuoto
+  let arrayNumbers = [];
+  /* fai il ciclo FINCHE' l'array non diventa
+   di 15 elementi + 1 elemento extra aggiunto sotto e si chiude */
+  while (arrayNumbers.length < 16) {
+    // dichiaro cost random associata alla funzione
+    const random = randomBomb(49);
+    // Se l'array non include questo numero
+    if (!arrayNumbers.includes(random)) {
+      // PUSHALO nell'array
+      arrayNumbers.push(random);
+    }
+  }
+  console.log(arrayNumbers);
 });
-
-/* L’utente indica un livello di difficoltà in base al quale viene
- generata una griglia di gioco quadrata, in cui ogni cella contiene
-  un numero tra quelli compresi in un range:
-con difficoltà 1 => tra 1 e 100
-con difficoltà 2 => tra 1 e 81
-con difficoltà 3 => tra 1 e 49
-Il computer deve generare 16 numeri casuali nello stesso range
- della difficoltà prescelta: le bombe.
-I numeri nella lista delle bombe non possono essere duplicati.
-In seguito l’utente clicca su una cella: se il numero è presente
- nella lista dei numeri generati - abbiamo calpestato una bomba - 
- la cella si colora di rosso e la partita termina, altrimenti la cella
-  cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
-La partita termina quando il giocatore clicca su una bomba o raggiunge
- il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, 
-cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. */
