31aee5635474e9df797f2e644b955ae817f661be
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-20 12:25:20 +0100

    Repo Created

--- JS/script.js
+++ JS/script.js
@@ -15,7 +15,7 @@ function easyBoxGenerator(container) {
   newBox.className = "square";
   // append al container
   container.append(newBox);
-//   container.innerText = num
+  //   container.innerText = num
   container.classList.remove("width-350");
   container.classList.remove("width-450");
   container.classList.add("width-500");
@@ -97,3 +97,23 @@ button2.addEventListener("click", function () {
 button3.addEventListener("click", function () {
   newGame(stampHtml, 49, hardBoxGenerator);
 });
+
+/* L’utente indica un livello di difficoltà in base al quale viene
+ generata una griglia di gioco quadrata, in cui ogni cella contiene
+  un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Il computer deve generare 16 numeri casuali nello stesso range
+ della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l’utente clicca su una cella: se il numero è presente
+ nella lista dei numeri generati - abbiamo calpestato una bomba - 
+ la cella si colora di rosso e la partita termina, altrimenti la cella
+  cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge
+ il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, 
+cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. */
+
+
